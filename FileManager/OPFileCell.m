fda5baccc782d963551020f086fe264ce599ef04
Oleg Pochtovy <opochtovy@yahoo.com>
2014-12-16 15:00:41 +0300

    Custom cell class for files section

---
+++ FileManager/OPFileCell.m
@@ -0,0 +1,23 @@
+//
+//  OPFileCell.m
+//  FileManager
+//
+//  Created by Oleg Pochtovy on 29.11.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+#import "OPFileCell.h"
+
+@implementation OPFileCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
