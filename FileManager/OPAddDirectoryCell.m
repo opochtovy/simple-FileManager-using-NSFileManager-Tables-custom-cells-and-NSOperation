1be5cc3776c5ec7ec89593e78ad2ee5e2a930df7
Oleg Pochtovy <opochtovy@yahoo.com>
2014-12-16 14:59:43 +0300

    Custom cell class for Add Directory button

---
+++ FileManager/OPAddDirectoryCell.m
@@ -0,0 +1,23 @@
+//
+//  OPAddDirectoryCell.m
+//  FileManager
+//
+//  Created by Oleg Pochtovy on 29.11.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+#import "OPAddDirectoryCell.h"
+
+@implementation OPAddDirectoryCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
