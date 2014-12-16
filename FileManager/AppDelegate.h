d169d2a645b6753d025ff324f2a912aa765dd973
Oleg Pochtovy <opochtovy@yahoo.com>
2014-12-16 14:55:59 +0300

    API illustrates blocks, work with NSFileManager, NSOperations and custom cells

---
+++ FileManager/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  FileManager
+//
+//  Created by Oleg Pochtovy on 27.11.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
