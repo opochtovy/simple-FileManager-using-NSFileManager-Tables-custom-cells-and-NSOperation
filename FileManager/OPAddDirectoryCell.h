15c00726e910e6574a182948c98bd263b8bea822
Oleg Pochtovy <opochtovy@yahoo.com>
2014-12-16 14:59:35 +0300

    Custom cell class for Add Directory button

---
+++ FileManager/OPAddDirectoryCell.h
@@ -0,0 +1,15 @@
+//
+//  OPAddDirectoryCell.h
+//  FileManager
+//
+//  Created by Oleg Pochtovy on 29.11.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPAddDirectoryCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UITextField *addDirectoryField;
+
+@end
